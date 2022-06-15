8644e7430f651fe89510d41661a33e983162aa1b
MickaelRigault <mick.rigault@gmail.com>
2022-06-15 15:06:44 +0200

    get_data inside classifications

--- ztfidr/typing.py
+++ ztfidr/typing.py
@@ -80,9 +80,51 @@ class Classifications( _DBApp_ ):
         self._data = self.data_from_db(sql="where kind='typing'")
         self._data["value"] = self._data["value"].str.strip().str.lower()
 
+    def load_sample(self):
+        """ """
+        from . import get_sample
+        self._sample = get_sample()
+        
     # -------- #
-    # PLOTTER  #
-    # -------- #    
+    # GETTER   #
+    # -------- #
+    def get_data(self, incl_unclear=True, min_classification=None,
+                     goodcoverage=None, redshift_range=None,
+                     x1_range=None, c_range=None, **kwargs):
+        """ 
+
+        min_classification: [None or int]
+            minimum number of typing for this target. 
+            unclear are not counted.
+
+        """
+        data = self.data.copy()
+        if not incl_unclear:
+            data = data[data["value"] != "unclear"]
+        if min_classification is not None and min_classification>0:
+            ntyped = self.get_ntyped(incl_unclear=False)
+            data = data[data["target_name"].isin(ntyped[ntyped>=min_classification].index)]
+            
+        # Sample
+        _locals = locals()
+        prop_sample = kwargs
+        for k in ["goodcoverage", "redshift_range", "x1_range", "c_range"]:
+            if _locals[k] is not None:
+                prop_sample[k] = _locals[k]
+                
+        if len(prop_sample) >0:
+            names_to_keep = self.sample.get_data(**prop_sample).index
+            data = data[data["target_name"].isin(names_to_keep)]
+            
+        return data
+        
+        
+            
+
+    def get_ntyped(self, incl_unclear=False):
+        """ series of the n"""
+        return self.get_data(incl_unclear=incl_unclear).groupby("target_name").size().sort_values()
+    
     def get_target_data(self, name):
         """ """
         return self.data.query(f"target_name == '{name}'")
@@ -126,3 +168,13 @@ class Classifications( _DBApp_ ):
         ax.set_xticks([])
         return fig
         
+    # ============== #
+    #  Properties    #
+    # ============== #
+    @property
+    def sample(self):
+        """ """
+        if not hasattr(self, "_sample"):
+            self.load_sample()
+            
+        return self._sample
