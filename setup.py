31fa91f8831d7558064e19550667bf2656fd8683
MickaelRigault <mick.rigault@gmail.com>
2024-08-02 15:53:43 +0200

    version up

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 packages = find_packages()
 
-VERSION = '0.12.0'
+VERSION = '0.13.0'
         
 setup(name='ztfidr',
       version=VERSION,
--- ztfidr/__init__.py
+++ ztfidr/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.12.0"
+__version__ = "0.13.0"
 
 from .sample import get_sample, get_data
 #from .lightcurve import *
